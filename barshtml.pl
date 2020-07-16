f4dd81b135b65f891ec8793db3b93b48436f2c0a
Felix J. Ogris <fjo@core7.intra.ogris.net>
2020-07-16 18:27:32 +0200

    WebGUI almost done

--- barshtml.pl
+++ barshtml.pl
@@ -3,12 +3,13 @@
 use strict;
 use warnings;
 
-foreach my $channel(qw(l r)) {
+foreach my $channel(qw(left right)) {
   for (my $band = 0; $band < 27; $band++) {
     print "<div class=\"band\">\n";
     for (my $bar = 24; $bar >= 0; $bar--) {
       print "<div id=\"$channel$band\_$bar\" class=\"bar\"></div>\n";
     }
+    print "<div id=\"$channel$band"."hz\" class=\"hz\"></div>\n";
     print "</div>"; # no newline to prevent space between inline-blocks
   }
 }
