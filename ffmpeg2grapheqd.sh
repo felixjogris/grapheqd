2714e815a1f10d38466e2f6ded38fa145ba3d8e8
Felix J. Ogris <fjo@core7.intra.ogris.net>
2021-05-09 21:12:22 +0200

    default to 2 channels, reset mixer on exit

--- ffmpeg2grapheqd.sh
+++ ffmpeg2grapheqd.sh
@@ -11,25 +11,22 @@ mixer =rec pcm2 >/dev/null 2>&1
 
 rate=""
 channels=""
-ac=""
-probe=`ffprobe -hide_banner /dev/dsp0 2>&1` || exit 1
+ac="2"
+probe=`ffprobe -hide_banner /dev/dsp0 2>&1`
 echo "$probe" | grep -q "44100 Hz" && rate='\104\254\0\0'
 echo "$probe" | grep -q "48000 Hz" && rate='\200\273\0\0'
 echo "$probe" | grep -q "mono"     && ac="1"
-echo "$probe" | grep -q "stereo"   && ac="2"
 if [ "$ac" = "1" ]; then
   channels='\1\0'
 elif [ "$ac" = "2" ]; then
   channels='\2\0'
 fi
-if [ -z "$rate" -o -z "$ac" -o -z "$channels" ]; then
+if [ -n "$rate" -a -n "$ac" -a -n "$channels" ]; then
+  printf "${channels}${rate}"
+  ffmpeg -loglevel quiet -i /dev/dsp0 -ac "$ac" -c:a pcm_s16le -f s16le - 2>/dev/null
+else
   (echo "Unsupported rate and/or channels:"
    echo "$probe") | logger -i -p daemon.info -t "$0"
-  exit 1
 fi
-
-printf "${channels}${rate}"
-
-ffmpeg -loglevel quiet -i /dev/dsp0 -ac "$ac" -c:a pcm_s16le -f s16le - 2>/dev/null
 
 mixer $mixersettings >/dev/null 2>&1
